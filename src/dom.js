2dbfaa6adfc31b4a84040387eb8e3b080a74722e
Brooks Susor <susorbrooks@gmail.com>
2022-10-04 16:03:42 -0400

    Update styles/add more dom logic

--- src/dom.js
+++ src/dom.js
@@ -1,12 +1,32 @@
 import { ToDo } from './createToDo.js'; 
 
+const divToDoContainer = document.querySelector('.todo-container'); 
+const aDefault = document.querySelector('.default-project');
+
 const btnCreate = document.querySelector('button').addEventListener('click', function() {
   const inputTitle = document.getElementById('title'); 
   const inputDescription = document.getElementById('description'); 
   const inputDueDate = document.getElementById('due-date'); 
   const inputPriority = document.getElementById('priority'); 
+  
+  const newToDo = new ToDo(
+    inputTitle.value, 
+    inputDescription.value, 
+    inputDueDate.value, 
+    inputPriority.value); 
+    
+  const divToDo = document.createElement('div')
+  divToDo.textContent = newToDo; 
+  divToDoContainer.appendChild(divToDo); 
+  }); 
 
-  const newToDo = new ToDo(inputTitle.value, inputDescription.value, inputDueDate.value, inputPriority.value); 
 
-  console.log(newToDo); 
+aDefault.addEventListener('click', function() {
+  while (divToDoContainer.children.length > 1) {
+    divToDoContainer.children[1].remove(); 
+  }
 }); 
+
+aDefault.addEventListener('click', function() {
+
+})
