ba767c715084eeb1a8ba0fa6b3feb04aa37b5a36
Brooks Susor <susorbrooks@gmail.com>
2022-10-04 19:22:50 -0400

    Still playing around

--- src/dom.js
+++ src/dom.js
@@ -4,29 +4,21 @@ const divToDoContainer = document.querySelector('.todo-container');
 const aDefault = document.querySelector('.default-project');
 
 const btnCreate = document.querySelector('button').addEventListener('click', function() {
-  const inputTitle = document.getElementById('title'); 
-  const inputDescription = document.getElementById('description'); 
-  const inputDueDate = document.getElementById('due-date'); 
-  const inputPriority = document.getElementById('priority'); 
+  let inputTitle = document.getElementById('title'); 
+  let inputDescription = document.getElementById('description'); 
+  let inputDueDate = document.getElementById('due-date'); 
+  let inputPriority = document.getElementById('priority'); 
   
-  const newToDo = new ToDo(
+  let newToDo = new ToDo(
     inputTitle.value, 
     inputDescription.value, 
     inputDueDate.value, 
     inputPriority.value); 
-    
-  const divToDo = document.createElement('div')
-  divToDo.textContent = newToDo; 
-  divToDoContainer.appendChild(divToDo); 
   }); 
 
 
 aDefault.addEventListener('click', function() {
   while (divToDoContainer.children.length > 1) {
-    divToDoContainer.children[1].remove(); 
+    divToDoContainer.children.remove(); 
   }
 }); 
-
-aDefault.addEventListener('click', function() {
-
-})
